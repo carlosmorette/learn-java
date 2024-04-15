e73d491e1bf9b35fc5b7e4c27cbffcd9289bc8fb
carlosmorette <carlos.morette@outlook.com>
2024-04-15 15:45:45 -0300

    add bookin-up-for-beauty

--- booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,24 +1,59 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.text.MessageFormat;
+import java.time.Month;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+	return LocalDateTime.parse(appointmentDateDescription, DateTimeFormatter.ofPattern("M/dd/yyyy HH:mm:ss"));
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+	var now = LocalDateTime.now();
+	return now.isAfter(appointmentDate);
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+	var hour = appointmentDate.getHour();
+	return hour >= 12 && hour < 18;
     }
 
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+	var dayOfWeek = appointmentDate.getDayOfWeek().toString();
+	var dayOfWeekString = capitalize(dayOfWeek);
+	var nameOfMonth = capitalize(appointmentDate.getMonth().toString());
+	var day = appointmentDate.getDayOfMonth();
+	var year = appointmentDate.getYear();
+	var appointTime = formatTime(appointmentDate);
+	return MessageFormat.format(
+				    "You have an appointment on {0}, {1} {2}, {3,number,#}, at {4}.",
+				       dayOfWeekString,
+				       nameOfMonth,
+				       day,
+				       year,
+				       appointTime		       
+				       );
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+	return LocalDate.of(LocalDate.now().getYear(), Month.SEPTEMBER, 15);
+    }
+
+    private String capitalize(String value) {
+	value = value.toLowerCase();
+	return value.substring(0, 1).toUpperCase() + value.substring(1);
+    }
+
+    private String formatTime(LocalDateTime dateTime) {
+	var hour = dateTime.getHour();
+	var hourFormatted = hour > 12 ? hour - 12 : hour;
+	var minute = fillMinutes(dateTime.getMinute());
+	var pmOrAm = dateTime.getHour() >= 12 ? "PM": "AM";
+	return MessageFormat.format("{0}:{1} {2}", hourFormatted, minute, pmOrAm);
+    }
+
+    private String fillMinutes(int minute) {
+	return minute <= 9 ? "0" + minute : String.valueOf(minute);
     }
 }
